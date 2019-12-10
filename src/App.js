febc2428dafe7482af56853944425fee289d36fa
FrancaTM <francatm@outlook.com>
2019-12-10 16:48:22 -0300

    flexible lists (updating state immutably)

--- src/App.js
+++ src/App.js
@@ -25,22 +25,19 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = event => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        {
-          name: "Max",
-          age: 28
-        },
-        {
-          name: event.target.value,
-          age: 29
-        },
-        {
-          name: "Stephanie",
-          age: 26
-        }
-      ]
+      persons: persons
     });
   };
 
@@ -81,6 +78,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 click={() => this.deletePersonHandler(index)}
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
